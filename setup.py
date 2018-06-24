7750ba3c9b5805b7a6a859ecec00f97be8bd0269
skj <shivkrishnajaiswal@gmail.com>
2018-06-24 17:04:43 +0530

    adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='exp',
+    version='0.1',
+    packages=['using_tensorflow'],
+    url='',
+    license='',
+    author='skj',
+    author_email='',
+    description=''
+)
